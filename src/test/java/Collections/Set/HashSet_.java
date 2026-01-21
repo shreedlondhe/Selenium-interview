501e6d0dc60f27e6bab2ee4b5ec769ee25be9f5f
shreedlondhe <shreedlondhe@gmail.com>
2026-01-21 08:13:19 +0530

    hash set

--- src/test/java/Collections/Set/HashSet_.java
+++ src/test/java/Collections/Set/HashSet_.java
@@ -24,6 +24,41 @@ public class HashSet_ {
 //            | Null allowed | One null      | One null        | No null |
 
 
+//    add(E e)
+//
+//    addAll(Collection<? extends E> c)
+//
+//    remove(Object o)
+//
+//    removeAll(Collection<?> c)
+//
+//    retainAll(Collection<?> c)
+//
+//    clear()
+//
+//    contains(Object o)
+//
+//    containsAll(Collection<?> c)
+//
+//    size()
+//
+//    isEmpty()
+//
+//    iterator()
+//
+//    forEach(Consumer<? super E> action)
+//
+//    toArray()
+//
+//    toArray(T[] a)
+//
+//    equals(Object o)
+//
+//    hashCode()
+//
+//    clone()
+//
+
     @Test
 void createHashSet(){
  HashSet <Integer>set=new HashSet<>(); // .get() method not for set cause it does not maintain insertion order
@@ -33,6 +68,37 @@ void createHashSet(){
 
       TreeSet tree=new TreeSet<>(set); // used for sorting
         System.out.println(tree);
+
+}
+
+@Test
+    void mergeTwoHashSet(){
+    HashSet<String> set1 = new HashSet<>();
+    set1.add("Java");
+    set1.add("Python");
+
+    HashSet<String> set2 = new HashSet<>();
+    set2.add("C");
+    set2.add("Java");  // duplicate, will be ignored
+
+    // Merge set2 into set1
+    set1.addAll(set2);
+
+    System.out.println(set1);
+}
+
+@Test
+    void setToArray(){
+    HashSet<String> set = new HashSet<>();
+    set.add("Java");
+    set.add("Python");
+
+    Object[] arr = set.toArray();
+
+    for (Object o : arr) {
+        System.out.println(o);
+    }
+
 
 }
 
