eebf9fb7e84fea882e79ddaa0d75ebbf3994d88b
shrinivas londhe <124325250+shreedlondhe@users.noreply.github.com>
2026-09-13 00:54:59 +0530

    Implement character and word analysis using HashMap
    Added multiple methods to find duplicates, non-repeated characters, and frequency counts using HashMap and HashSet. Included sorting methods for HashMap by keys and values.

--- src/test/java/Collections/Map/HashMap_.java
+++ src/test/java/Collections/Map/HashMap_.java
@@ -83,5 +83,219 @@ for(String i:str.split("")){
 map.put(i,map.getOrDefault(i,0)+1);
 }
 System.out.println(map);
+
+    // Find duplicate characters ⭐
+    //using HashMap
+public static void main(String[] args) {
+String str = "programming";
+HashMap<String,Integer>map= new HashMap<>();
+for(String i:str.split("")){
+map.put(i,map.getOrDefault(i,0)+1);
 }
+System.out.println(map);
+
+for(String j:map.keySet()){
+ if(map.get(j)>1){
+  System.out.println("Duplicate char is "+ j);
+ }
+}
+}
+    //using HashSet
+public static void main(String[] args) {
+String str = "programming";
+HashSet<String> set=new HashSet<>();
+HashSet<String> DuplicateSet=new HashSet<>();
+
+for(String s:str.split("")){
+  if(!set.add(s)){
+    DuplicateSet.add(s);
+  }
+}
+System.out.println(DuplicateSet);
+}
+
+    // Find first non-repeated character ⭐
+    
+public static void main(String[] args) {
+String str = "programming";
+HashMap<String,Integer>map=new HashMap<>();
+for(String s:str.split("")){
+ map.put(s,map.getOrDefault(s,0)+1);
+}
+for(String j:str.split("")){
+ if(map.get(j)==1){
+  System.out.println("First non reapeated char is "+j);
+  break;
+ }
+}
+}
+
+    //Find first repeated character ⭐
+    
+public static void main(String[] args) {
+String str = "programming";
+HashMap<String,Integer>map=new HashMap<>();
+for(String s:str.split("")){
+ map.put(s,map.getOrDefault(s,0)+1);
+}
+
+for(String j:str.split("")){
+ if(map.get(j)>1){
+  System.out.println("First non reapeated char is "+j);
+  break;
+ }
+}
+}   
+
+    
+    //Count frequency of words ⭐
+
+public static void main(String[] args) {
+String str = "java selenium java api selenium java";
+HashMap<String,Integer> map=new HashMap<>();
+for(String s:str.split(" ")){
+map.put(s,map.getOrDefault(s,0)+1);
+}
+System.out.println(map);
+}
+
+
+    // Find duplicate words
+
+public static void main(String[] args) {
+String str = "java selenium java api selenium java";
+HashMap<String,Integer> map=new HashMap<>();
+for(String s:str.split(" ")){
+map.put(s,map.getOrDefault(s,0)+1);
+}
+for(String i:map.keySet()){
+  if(map.get(i)>1){
+  System.out.println("Duplicate Word is "+i);
+  }
+}
+}
+
+
+    //Find maximum value in HashMap ⭐
+
+
+public static void main(String[] args) {
+Map<String, Integer> map = new HashMap<>();
+map.put("A", 50);
+map.put("B", 90);
+map.put("C", 70);
+int  max=new ArrayList<>(map.values()).get(0);
+for(String i:map.keySet()){
+  if(map.get(i)>max){
+    max=map.get(i);
+  }
+}
+System.out.println("Max value is "+max);
+}
+
+    //Find minimum value in  HashMap ⭐
+public static void main(String[] args) {
+Map<String, Integer> map = new HashMap<>();
+map.put("A", 50);
+map.put("B", 90);
+map.put("C", 70);
+int  max=new ArrayList<>(map.values()).get(0);
+for(String i:map.keySet()){
+  if(map.get(i)<max){
+    max=map.get(i);
+  }
+}
+System.out.println("Min value is "+max);
+}   
+}
+
+  // Find key having maximum value ⭐
+public static void main(String[] args) {
+Map<String, Integer> map = new HashMap<>();
+map.put("A", 50);
+map.put("B", 90);
+map.put("C", 70);
+int  max=new ArrayList<>(map.values()).get(0);
+
+for(String i:map.keySet()){
+  if(map.get(i)>max){
+    max=map.get(i);
+  }
+}
+System.out.println(max);
+for(String j:map.keySet()){
+  if(map.get(j)==max){
+    System.out.println("Key for max value is "+j);
+  }
+}
+}
+
+
+    //Find key having minimum value
+    
+public static void main(String[] args) {
+Map<String, Integer> map = new HashMap<>();
+map.put("A", 50);
+map.put("B", 90);
+map.put("C", 70);
+int  min=new ArrayList<>(map.values()).get(0);
+
+for(String i:map.keySet()){
+  if(map.get(i)<min){
+    min=map.get(i);
+  }
+}
+System.out.println(min);
+for(String j:map.keySet()){
+  if(map.get(j)==min){
+    System.out.println("Key for min value is "+j);
+  }
+}
+}
+
+
+    // Sort HashMap by keys ⭐
+// first method
+Map<Integer, String> map = new HashMap<>();
+map.put(3, "C");
+map.put(1, "A");
+map.put(2, "B");
+Map<Integer, String> sortedMap = new TreeMap<>(map);
+System.out.println(sortedMap);
+//second method
+public static void main(String[] args) {
+Map<String, Integer> map = new HashMap<>();
+map.put("C", 70);
+map.put("D", 50);
+map.put("A", 50);
+map.put("B", 90);
+List<String>keysList=new ArrayList<>(map.keySet());
+Collections.sort(keysList);
+for(String s:keysList){
+System.out.println(s+":"+map.get(s));
+}
+}
+
+
+ // Sort HashMap by values ⭐
+public static void main(String[] args) {
+Map<String, Integer> map = new HashMap<>();
+map.put("C", 70);
+map.put("D", 20);
+map.put("A", 50);
+map.put("B", 90);
+List<Integer>valuesList=new ArrayList<>(map.values());
+Collections.sort(valuesList);
+for(int s:valuesList){
+ for(String j:map.keySet()){
+       if(map.get(j)==s){
+     System.out.println(j+":"+s);
+       }
+ }
+}
+}
+
+
+    
+    
 }
