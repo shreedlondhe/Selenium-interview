8e175773b77151d2f7ecc4554e23ea72eff5c563
shrinivas londhe <124325250+shreedlondhe@users.noreply.github.com>
2026-09-12 22:37:58 +0530

    Update HashMap_.java

--- src/test/java/Collections/Map/HashMap_.java
+++ src/test/java/Collections/Map/HashMap_.java
@@ -21,4 +21,67 @@ public class HashMap_ {
                 System.out.println(ts);
 
     }
+
+ // Create and iterate HashMap    :  method map.keySet()  return the set of keys
+    public static void main(String[] args) {
+       LinkedHashMap<String,Integer>map= new LinkedHashMap<>();
+            map.put("D",4);
+            map.put("A",1);
+            map.put("B",2);
+             map.put("C",3);
+        for(String s:map.keySet())
+          {
+             System.out.println(s+ "  "+map.get(s));
+           }
+       }
+
+    
+  // Check whether key exists       method= map.ContainsKey(<your_key>)  returns return boolean value
+    
+public static void main(String[] args) {
+LinkedHashMap<String,Integer>map= new LinkedHashMap<>();
+map.put("D",4);
+map.put("A",1);
+map.put("B",2);
+map.put("C",3);
+System.out.println(map.containsKey("Z")?"Contains key":"Not contained key");
+System.out.println(map.containsKey("A")?"Contains key":"Not contained key");
+} 
+
+    // Check whether value exists  method= map.containsValue(<your_key>)  returns return boolean value
+
+public static void main(String[] args) {
+LinkedHashMap<String,Integer>map= new LinkedHashMap<>();
+map.put("D",4);
+map.put("A",1);
+map.put("B",2);
+map.put("C",3);
+System.out.println(map.containsValue(4)?"Contains value":"Not contained value");
+System.out.println(map.containsValue(8)?"Contains value":"Not contained value");
+}
+
+    //Get value using key    method= map.get(<YourKey>)
+    //  System.out.println(map.get("A"));
+
+    // Get value with default value  methods=  map.getOrDefault(<YourKey>, 0)  return default value of key
+    public static void main(String[] args) {
+LinkedHashMap<String,Integer>map= new LinkedHashMap<>();
+map.put("D",4);
+map.put("A",6);
+map.put("B",2);
+map.put("C",3);
+System.out.println(map.getOrDefault("A", 0));
+}
+
+
+    // Count frequency of characters ⭐
+
+public static void main(String[] args) {
+String str = "programming";
+HashMap<String,Integer>map= new HashMap<>();
+for(String i:str.split("")){
+map.put(i,map.getOrDefault(i,0)+1);
+}
+System.out.println(map);
+}
 }
