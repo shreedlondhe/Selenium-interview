1b165b3cb554c7e9bcfe5bbdce92173894be7de8
shrinivas londhe <124325250+shreedlondhe@users.noreply.github.com>
2026-09-13 15:05:36 +0530

    Update Collections_and_It_methods.java

--- src/test/java/Collections/Collection/Collections_and_It_methods.java
+++ src/test/java/Collections/Collection/Collections_and_It_methods.java
@@ -19,4 +19,33 @@ public class Collections_and_It_methods {
 //                                       Store object Wrapper classes
 //                                       Easy data handling
 
+
+
+// // Collections methods
+// | Sr. No. | Method           | Example                                                                           | Output                          |
+// | ------: | ---------------- | --------------------------------------------------------------------------------- | ------------------------------- |
+// |       1 | `addAll()`       | `Collections.addAll(list, 10, 20, 30);`                                           | `[10, 20, 30]`                  |
+// |       2 | `max()`          | `Collections.max(Arrays.asList(10, 30, 20));`                                     | `30`                            |
+// |       3 | `min()`          | `Collections.min(Arrays.asList(10, 30, 20));`                                     | `10`                            |
+// |       4 | `sort()`         | `Collections.sort(list);` where `list = [30, 10, 20]`                             | `[10, 20, 30]`                  |
+// |       5 | `reverse()`      | `Collections.reverse(list);` where `list = [10, 20, 30]`                          | `[30, 20, 10]`                  |
+// |       6 | `reverseOrder()` | `Collections.sort(list, Collections.reverseOrder());` where `list = [10, 30, 20]` | `[30, 20, 10]`                  |
+// |       7 | `frequency()`    | `Collections.frequency(Arrays.asList(10, 20, 10, 30), 10);`                       | `2`                             |
+// |       8 | `swap()`         | `Collections.swap(list, 0, 2);` where `list = [10, 20, 30]`                       | `[30, 20, 10]`                  |
+// ----------------------------------------------------------------------------------------------------------------------------------------------------
+
+// //Arrrays methods
+//   | Sr. No. | Method       | Example                                                                              | Output             |
+// | ------: | ------------ | ------------------------------------------------------------------------------------ | ------------------ |
+// |       1 | `sort()`     | `int[] arr = {30, 10, 20};`<br>`Arrays.sort(arr);`                                   | `[10, 20, 30]`     |
+// |       2 | `equals()`   | `int[] arr1 = {10, 20};`<br>`int[] arr2 = {10, 20};`<br>`Arrays.equals(arr1, arr2);` | `true`             |
+// |       3 | `toString()` | `int[] arr = {10, 20, 30};`<br>`Arrays.toString(arr);`                               | `[10, 20, 30]`     |
+// |       4 | `asList()`   | `String[] arr = {"Java", "Selenium"};`<br>`Arrays.asList(arr);`                      | `[Java, Selenium]` |
+// |       5 | `stream()`   | `int[] arr = {10, 20, 30};`<br>`Arrays.stream(arr).sum();`                           | `60`               |
+// --------------------------------------------------------------------------------------------------------------------------------------
+
+
+  
+
+  
 }
