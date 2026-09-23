11f76b57ee5ca3fb902844d40c2794a62a16da93
shrinivas londhe <124325250+shreedlondhe@users.noreply.github.com>
2026-09-23 21:37:38 +0530

    Add function to rearrange negative numbers in array
    Implemented a function to move all negative numbers to the start of the array.

--- src/test/java/JavaPrograms/ArrayPrograms.java
+++ src/test/java/JavaPrograms/ArrayPrograms.java
@@ -607,6 +607,25 @@ if(left-right==1){
 }
 }
 
+
+// move all negavtive to start or end
+let arr = [1, -2, 3, -4, 5, -6];
+// [1, -2, 3, -4, 5, -6]
+// [-2,1,3,-4,5,-6]
+//[-2,-4,3,1,5,-6]
+// [-2,-4,-6,1,5,3]
+let j=0
+for(let i=0;i<arr.length;i++){
+
+if(arr[i]<0){
+    let temp=arr[j]
+    arr[j]=arr[i]
+    arr[i]=temp
+j++
+}
+}
+console.log(arr)
+      
     }
 
 
